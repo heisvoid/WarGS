6607f083268605ddf45d9ee6b5e32623eea4d815
heisvoid <heisvoid@gmail.com>
2011-07-16 23:10:19 +0900

    Replace open () with the wrapper one in ttl

--- src/wargs-ttl.c
+++ src/wargs-ttl.c
@@ -1,10 +1,19 @@
 /* vim: set sw=2 ts=2 expandtab: */
 
+#include "conf.h"
+#include "filepath.h"
+
 int
 main ()
 {
+  conf_init ();
+  filepath_init (conf_get_root ());
+
   int ret = 0;
   asm volatile ("call ttl" : "=a" (ret));
+
+  filepath_quit ();
+  conf_quit ();
 
   return ret;
 }
