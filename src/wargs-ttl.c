4c0dd4bcf1ae21425b117327a7f24358ccbdf026
heisvoid <heisvoid@gmail.com>
2011-07-19 10:29:27 +0900

    Enable video in ttl

--- src/wargs-ttl.c
+++ src/wargs-ttl.c
@@ -2,16 +2,19 @@
 
 #include "conf.h"
 #include "filepath.h"
+#include "video.h"
 
 int
 main ()
 {
   conf_init ();
   filepath_init (conf_get_root ());
+  video_init (conf_get_ratio ());
 
   int ret = 0;
   asm volatile ("call ttl" : "=a" (ret));
 
+  video_quit ();
   filepath_quit ();
   conf_quit ();
 
