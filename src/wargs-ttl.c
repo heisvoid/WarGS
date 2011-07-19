aa32d93eb72a1cbdd4c456704f5779df7cf6911c
heisvoid <heisvoid@gmail.com>
2011-07-19 13:19:00 +0900

    Add audio system in ttl

--- src/wargs-ttl.c
+++ src/wargs-ttl.c
@@ -1,21 +1,41 @@
 /* vim: set sw=2 ts=2 expandtab: */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+
 #include "conf.h"
 #include "filepath.h"
 #include "video.h"
 #include "keyboard.h"
+#include "audio.h"
+#include "util.h"
 
 int
 main ()
 {
   conf_init ();
-  filepath_init (conf_get_root ());
+
+  const char * const root = conf_get_root ();
+
+  filepath_init (root);
   video_init (conf_get_ratio ());
   keyboard_init ();
+
+  const size_t music_root_size = sizeof (char) * (strlen (root) + 1 + 5 + 1);
+  char * const music_root = xmalloc (music_root_size);
+  xsnprintf (music_root, music_root_size,
+             "%s%cmusic", root, FILEPATH_SEPARATOR);
+  audio_init (music_root);
+  free (music_root);
 
   int ret = 0;
   asm volatile ("call ttl" : "=a" (ret));
 
+  audio_quit ();
   keyboard_quit ();
   video_quit ();
   filepath_quit ();
