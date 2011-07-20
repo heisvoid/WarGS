c8a4324268649c9d6765664fd28f7fefaa6bdc89
heisvoid <heisvoid@gmail.com>
2011-07-20 17:13:00 +0900

    Improve game.cfg reading routine

--- src/game.c
+++ src/game.c
@@ -28,18 +28,9 @@ static uint32_t fps = DEFAULT_FPS;
 void
 game_cfg_setup ()
 {
-#ifdef TTL
-  extern int8_t game_cfg;
+  extern int16_t game_cfg_speed;
 
-  (&game_cfg)[0] = (true == conf_get_fast () ? 1 : 0);
-  (&game_cfg)[1] = 1; /* force music on */
-  (&game_cfg)[2] = 1; /* force sound on */
-  (&game_cfg)[3] = 5; /* unknown */
-  ((int16_t *) (&game_cfg))[2] = 220; /* force sound-blaster ADR 220 */
-  ((int16_t *) (&game_cfg))[3] = 7;   /* force sound-blaster IRQ 7 */
-  ((int16_t *) (&game_cfg))[4] = 1;   /* force sound-blaster DMA 1 */
-#elif defined(COMBATII)
-#endif
+  game_cfg_speed = (true == conf_get_fast () ? 1 : 0);
 }
 
 void
