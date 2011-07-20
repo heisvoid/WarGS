329c448674f3b092c33058317c3fb267f84b3a49
heisvoid <heisvoid@gmail.com>
2011-07-20 12:44:31 +0900

    Pass command line arguments (argc, argv) to combatii

--- src/wargs-combatii.c
+++ src/wargs-combatii.c
@@ -1,10 +1,10 @@
 /* vim: set sw=2 ts=2 expandtab: */
 
 int
-main ()
+main (int argc, char *argv[])
 {
   int ret = 0;
-  asm volatile ("call combatii" : "=a" (ret));
+  asm volatile ("call combatii" : "=a" (ret) : "a" (argc), "d" (argv));
 
   return ret;
 }
