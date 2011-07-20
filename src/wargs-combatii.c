a625f49684ac10cc4da6c03d4b7c028776f72b6f
heisvoid <heisvoid@gmail.com>
2011-07-20 17:16:41 +0900

    Add game init/quit routine in wargs-combatii

--- src/wargs-combatii.c
+++ src/wargs-combatii.c
@@ -1,10 +1,16 @@
 /* vim: set sw=2 ts=2 expandtab: */
 
+#include "game.h"
+
 int
 main (int argc, char *argv[])
 {
+  game_init ();
+
   int ret = 0;
   asm volatile ("call combatii" : "=a" (ret) : "a" (argc), "d" (argv));
+
+  game_quit ();
 
   return ret;
 }
